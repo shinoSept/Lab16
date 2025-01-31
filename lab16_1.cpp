4d37c841e0681401127d213cc2eb72fd207b31af
Chinoros <chinoros2815@gmail.com>
2025-01-31 17:17:31 +0700

    edited

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,41 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *dPtr, int N, int M) {
+	cout << fixed << setprecision(2);
+	for (int i = 0; i < N*M; i++) {
+		cout << *(dPtr+i);
+		if ((i+1) % M == 0 ) cout << endl;
+		else cout << " ";
+
+	}
+}
+
+void randData(double *dPtr, int N, int M) {
+	for (int i = 0; i < N*M; i++) {
+		*(dPtr+i) = double(rand() % 101) / 100;
+	}
+}
+
+void findRowSum(const double *dPtr,double *sum1, int N, int M) {
+    double sum = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			sum += *(dPtr + j + (i*M));
+		}
+		sum1[i] = sum;
+		sum = 0;
+	}
+}
+
+void findColSum(const double *dPtr,double *sum2, int N, int M) {
+    double sum = 0;
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) {
+			sum += *(dPtr + (j*M) + i);
+		}
+		sum2[i] = sum;
+		sum = 0;
+	}
+}
